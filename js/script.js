5dab878638d0712bf70ce7226a3820489b784316
Emilien Cuny <emilien.cuny0312@gmail.com>
2022-12-21 16:05:12 +0100

    ajout register fonctionnel(popup)

--- js/script.js
+++ js/script.js
@@ -13,11 +13,27 @@ function openForm(id) {
 }
 
 function nextstep() {
-  document.getElementById('first-step').style.display = "none"
-  document.getElementById('second-step').style.display = "block"
+  document.getElementById('first-step').style.display = "none";
+  document.getElementById('second-step').style.display = "block";
+  document.getElementById('first-step-buttons').style.display = "none";
+  document.getElementById('second-step-buttons').style.display = "block";
 }
 
 function previousstep(){
-  document.getElementById('first-step').style.display = "block"
-  document.getElementById('second-step').style.display = "none"
+  document.getElementById('first-step').style.display = "block";
+  document.getElementById('second-step').style.display = "none";
+  document.getElementById('first-step-buttons').style.display = "block";
+  document.getElementById('second-step-buttons').style.display = "none";
+}
+
+function afficher_image(){
+  var img = document.createElement("img");
+  img.src = '../avatar/avatars_amy.png';
+  var block = document.getElementById("x");
+  block.appendChild(img);
+}
+
+function changeAvatar(){
+  var text=document.getElementById('f_selectTrie').options[document.getElementById('f_selectTrie').selectedIndex].value;
+  document.getElementById('avatar').src=text;
 }
