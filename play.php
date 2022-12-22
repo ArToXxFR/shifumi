c33511074c423a11b3634b723f4d5fb10d7c8266
Nicolas <nicolas.guillemin@epsi.fr>
2022-12-22 15:52:25 +0100

    Styliser pages jouer (début)

--- play.php
+++ play.php
@@ -3,8 +3,8 @@
 require "connect_bdd.php";
 $jeu_lance = false;
 
-if(isset($_SESSION['pseudo']) && isset($_SESSION['image'])){
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if (isset($_SESSION['pseudo']) && isset($_SESSION['image'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $jeu_lance = true;
         switch ($_POST['shifumi']) {
             case 'pierre':
@@ -26,10 +26,11 @@ if(isset($_SESSION['pseudo']) && isset($_SESSION['image'])){
     header('Location: index.php');
 }
 
-function userConnected(){
-    if(isset($_SESSION['pseudo']) && isset($_SESSION['image'])){
+function userConnected()
+{
+    if (isset($_SESSION['pseudo']) && isset($_SESSION['image'])) {
         return true;
-    } else{
+    } else {
         return false;
     }
 }
@@ -40,6 +41,7 @@ function userConnected(){
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -48,43 +50,51 @@ function userConnected(){
     <link href="css/style.css" rel="stylesheet">
     <script src="js/script.js"></script>
 </head>
+
 <body>
     <?php require('header.php'); ?>
-    <?php if(!$jeu_lance){ ?>
-    <span> Choisis l'un des 3 signes :</span>
+    <?php if (!$jeu_lance) { ?>
+        <span class="choice"> Choisis l'un des 3 signes :</span>
 
-    <form method="POST" action="play.php" onsubmit="return valider_jeu()">
-        <div>
-            <div>
-                <p>Pierre</p>
-                <img src="icons_jeu/pierre.png" alt="pierre">
-                <input type="radio" name="shifumi" value="pierre" id="pierre" class="input-hidden" required>
-                <label for="pierre" class="radio"><div class="inside-radio"></div></label>
+        <form method="POST" action="play.php" onsubmit="return valider_jeu()">
+            <div class="container">
+                <div class="box">
+                    <p class="mots-gras">Pierre</p>
+                    <img src="icons_jeu/pierre.png" alt="pierre">
+                    <input type="radio" name="shifumi" value="pierre" id="pierre" class="input-hidden" required>
+                    <label for="pierre" class="radio">
+                        <div class="inside-radio"></div>
+                    </label>
+                </div>
+                <div class="box">
+                    <p class="mots-gras">Papier</p>
+                    <img src="icons_jeu/papier.png" alt="papier">
+                    <input type="radio" name="shifumi" value="papier" id="papier" class="input-hidden" required>
+                    <label for="papier" class="radio">
+                        <div class="inside-radio"></div>
+                    </label>
+                </div>
+                <div class="box">
+                    <p class="mots-gras">Ciseaux</p>
+                    <img src="icons_jeu/ciseaux.png" alt="ciseaux">
+                    <input type="radio" name="shifumi" value="ciseaux" id="ciseaux" class="input-hidden" required>
+                    <label for="ciseaux" class="radio">
+                        <div class="inside-radio"></div>
+                    </label>
+                </div>
             </div>
-            <div>
-                <p>Papier</p>
-                <img src="icons_jeu/papier.png" alt="papier">
-                <input type="radio" name="shifumi" value="papier" id="papier" class="input-hidden" required>
-                <label for="papier" class="radio"><div class="inside-radio"></div></label>
-            </div>
-            <div>
-                <p>Ciseaux</p>
-                <img src="icons_jeu/ciseaux.png" alt="ciseaux">
-                <input type="radio" name="shifumi" value="ciseaux" id="ciseaux" class="input-hidden" required>
-                <label for="ciseaux" class="radio"><div class="inside-radio"></div></label>
-            </div>
-        </div>
-        <input type="submit" value="Valider mon choix" class="open-button jaune">
-    </form>
-    <button onclick="document.location.href='index.php'" class="open-button rouge-pastel">Annuler la partie</button>
+            <input type="submit" value="Valider mon choix" class="open-button jaune">
+        </form>
+        <button onclick="document.location.href='index.php'" class="open-button rouge-pastel">Annuler la partie<img class="icon-button" src="img/fleche.svg" alt="flèche"> </button>
 
     <?php } else { ?>
-        <span> C'est parti... </span>
-        <span> SHI... FU... MI... </span>
-        <img src="<?= $_SESSION['image']?>" alt="profil joueur" class="icons">
+        <span class="choice"> C'est parti... </span>
+        <span class="choice"> SHI... FU... MI... </span>
+        <img src="<?= $_SESSION['image'] ?>" alt="profil joueur" class="icons">
         <img src="<?= $icon ?>" alt="icon jeu">
     <?php } ?>
     <?php require('footer.php'); ?>
 
 </body>
+
 </html>
