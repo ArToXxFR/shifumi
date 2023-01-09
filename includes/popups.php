9766bc97d23d9c589039f3eee56a12f6e3437108
Gregoire GAUTIER <gregoiregautier@ymail.com>
2023-01-09 13:35:37 +0100

    CSS Greg

--- includes/popups.php
+++ includes/popups.php
@@ -11,26 +11,19 @@
 <div class="position-popup" id="popupForm">
     <div class="form-popup">
         <h2 class="title-popup rouge-pastel">Rappel des Règles</h2>
-        <p>À chaque partie, le joueur choisit l'une de ces trois actions</p>
-        <ul>
-            <li>pierre</li>
-            <li>papier</li>
-            <li>ciseaux</li>
-        </ul>
-        <ol>
-            <li><img class="taille-image" src="/icons_jeu/pierre.png" alt="icon pierre"></li>
-            <li><img class="taille-image" src="/icons_jeu/papier.png" alt="icon papier"></li>
-            <li><img class="taille-image" src="/icons_jeu/ciseaux.png" alt="icon ciseaux"></li>
-        </ol>
-        <p>La <strong class="mots_gras">pierre</strong> bat les <strong class="mots_gras">ciseaux</strong> en les émoussant.</p>
-        <p>Le <strong class="mots_gras">papier</strong> bat la <strong class="mots_gras">pierre</strong> en l'enveloppant.</p>
-        <p>Les <strong class="mots_gras">ciseaux</strong> battent le <strong class="mots_gras">papier</strong> en la coupant.</p>
+        <p>À chaque partie, le joueur choisit l'une ces trois actions</p>
+        <p class="regles_signes pierre mots-gras">pierre</p>
+        <p class="regles_signes papier mots-gras">papier</p>
+        <p class="regles_signes ciseaux mots-gras">ciseaux</p>
+        <p>La <strong class="mots-gras">pierre</strong> bat les <strong class="mots-gras">ciseaux</strong> en les émoussant.</p>
+        <p>Le <strong class="mots-gras">papier</strong> bat la <strong class="mots-gras">pierre</strong> en l'enveloppant.</p>
+        <p>Les <strong class="mots-gras">ciseaux</strong> battent le <strong class="mots-gras">papier</strong> en la coupant.</p>
         <p>
             Il peut y avoir des matchs nulles si le joueur et Bender choisissent
             la même action.
         </p>
     </div>
-    <button class="close-popup rouge-pastel" onclick="closeForm('popupForm')">✖</button>
+    <button class="close-popup rouge-pastel" onclick="closeForm('popupForm')"></button>
 </div>
 
 <!------------------------------------------------------------
@@ -45,7 +38,7 @@
         <form action="index.php" method="POST" class="flex-login">
             <input name="username" type="text" maxlength="25" placeholder="Nom d'utilisateur / Adresse email" class="input-login">
             <input name="password" type="password" placeholder="Mot de passe" class="input-login">
-            <span class="mots_gras mots_de_passe_oublie">Mot de passe oublié ?</span>
+            <span class="mots_gras">Mot de passe oublié ?</span>
             <input name="button-login" type="submit" value="Se connecter" class="button-popup cyan">
             <input type="hidden" name="name" value="login">
         </form>
@@ -77,7 +70,7 @@
                         <option value="<?= $avatar['image'] ?>"><?= $avatar['nom'] ?></option>
                     <?php } ?>
                 </select>
-                <img src="/medias/avatars/avatars_fry.png" alt="avatar" id="avatar" width="50px">
+                <img src="medias/avatars/avatars_fry.png" alt="avatar" id="avatar" width="50px">
                 <input type="submit" name="button-register" value="Terminer">
             </div>
         </form>
@@ -103,7 +96,7 @@
         <span class="choice">Etes-vous sur de vouloir vous déconnecter ?</span>
         <button onclick="document.location.href='/deconnection.php'" class="button-popup rouge-pastel">Oui je veux me déconnecter</button>
         <button onclick="document.location.href='/index.php'" class="button-popup jaune">C'était une erreur, annuler</button>
-        <img src="/medias/bender_message/bender_terminator.svg" alt="Au revoir de bender">
+        <img src="/medias/bender_message/bender_terminator.png" alt="Au revoir de bender">
     </div>
     <button class="close-popup rouge-pastel" onclick="closeForm('deconnection')">✖</button>
 </div>
