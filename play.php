f04dd700b5624881752dd6c5fcb15448f2d2ba59
Emilien Cuny <emilien.cuny0312@gmail.com>
2023-01-09 09:28:13 +0100

    commit

--- play.php
+++ play.php
@@ -34,7 +34,7 @@ if (isset($_SESSION['pseudo']) && isset($_SESSION['image'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jeu shifumi</title>
     <link href="css/style.css" rel="stylesheet">
     <script src="js/script.js"></script>
 </head>
