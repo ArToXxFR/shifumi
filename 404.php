57cf7a1f4f04efc0ab4dd2b9eaa1e38fad0281a8
Emilien Cuny <emilien.cuny0312@gmail.com>
2023-01-16 11:52:29 +0100

    page 404

---
+++ 404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <?php require "includes/header.php"; ?>
+    <div class="">
+        <h1>Erreur 404</h1>
+        <span>La page que vous cherchez semble introuvable.</span>
+    </div>
+    <button href="index.php">Revenir à la page d'accueil</button>
+    <button href="javascript:history.go(-1)">Revenir à la page précédente</button>
+    <img src="medias/bender_message/bender_404.png" class="" alt="Bender 404">
+</body>
+</html>
